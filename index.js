33306976a84a89ba2f51fafbe98a438352f84811
whitney-higgins <whitneyxhiggins@gmail.com>
2023-03-10 23:17:34 -0600

    completed tasks 1 and 2

--- index.js
+++ index.js
@@ -209,16 +209,18 @@ Practice accessing data above by console.log-ing following items:
 
 //(1) Name of the first artist (0th index) in the array
 
-
+//console.log(artists[0])
 
 //(2) Bio of the third artist (2nd index) in the array 
 
-
+//console.log(artists[2]["bio"])
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2 (not auto tested): 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 (no function needed) 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Fix this issue and console.log() to check your work. */
+artists[8]["name"] = "Vincent Van Gogh"
 
+//console.log(artists[8])
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
